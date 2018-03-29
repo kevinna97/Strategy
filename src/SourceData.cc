3b7c14d47057ce6166189fd30a412d3716d94693
kevinna97 <kevinna2019@gmail.com>
2018-03-28 23:35:05 -0500

    Update SourceData.cc

--- src/SourceData.cc
+++ src/SourceData.cc
@@ -1,1 +1,12 @@
+#include "SourceData.h"
+
+using namespace std;
+
+SourceData::SourceData(int * Data, String SourceFile, Board * pBoard): SourceFile{SourceFile}, pBoard{pBoard}{
+  int Data[19];
+}
+
+int SourceData::Getdata(int num){
+  return Data[num];
+}
 
