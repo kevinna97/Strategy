7918e583093e902ddb45cae11890cf3a2a13c26b
kevinna97 <kevinna2019@gmail.com>
2018-03-29 19:37:36 -0500

    Update SourceData.cc

--- src/SourceData.cc
+++ src/SourceData.cc
@@ -1,9 +1,38 @@
 #include "SourceData.h"
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+SourceData::SourceData(): SourceFile{"DefaultLayout.txt"}{
+  int Data[19];
+  ifstream File;
+  File.open (SourceFile);
+  string word;
+  char x;
+  int counter = 0;
+  
+  while(File>>word){
+    Data[counter] = word;
+    counter++;
+  }
+}
+    
+  
+}
+
 SourceData::SourceData(int * Data, String SourceFile, Board * pBoard): SourceFile{SourceFile}, pBoard{pBoard}{
   int Data[19];
+  ifstream File;
+  File.open (SourceFile);
+  string word;
+  char x;
+  int counter = 0;
+  
+  while(File>>word){
+    Data[counter] = word;
+    counter++;
+  }
 }
 
 int SourceData::Getdata(int num){
